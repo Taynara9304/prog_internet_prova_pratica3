902ab478b4f62b755bed91e0f8a20eea180fa234
Taynara <taynaradasilva2001@gmail.com>
2025-09-22 11:52:34 -0300

    final

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,21 +1,19 @@
 import jwt from 'jsonwebtoken';
 
 function authMiddleware(req, res, next) {
-        // Obter o token do cabeçalho da requisição (normalmente na propriedade Authorization)
         const authHeader = req.headers['authorization'];
-        const token = authHeader && authHeader.split(' ')[1]; // Formato esperado: Bearer TOKEN
+        const token = authHeader && authHeader.split(' ')[1];
       
         if (token == null) {
-          return res.sendStatus(401); // Se não houver token, retornar 401 Unauthorized
+          return res.sendStatus(401);
         }
       
         jwt.verify(token, "secreta123", (err, user) => {
           if (err) {
-            // Se o token for inválido ou expirado
             return res.status(401).json({ message: "Token inválido ou expirado" });
           }
-          req.user = user; // Anexa o payload (informações do usuário) ao objeto request
-          next(); // Prossegue para a próxima função de middleware ou para o manipulador da rota
+          req.user = user;
+          next();
         });
 };
 
